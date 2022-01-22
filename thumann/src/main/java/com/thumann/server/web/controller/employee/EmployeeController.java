822671fdff1e9ab64febbaab45cf7191affd51cb
2man2man <sebastian.thumann@kabelmail.de>
2022-01-22 15:36:35 +0100

    safdg

--- thumann/src/main/java/com/thumann/server/web/controller/employee/EmployeeController.java
+++ thumann/src/main/java/com/thumann/server/web/controller/employee/EmployeeController.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PathVariable;
-=======
->>>>>>> branch 'master' of https://github.com/2man2man/serverProject.git
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
